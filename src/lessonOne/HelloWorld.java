64cd149af69454c71a6a996246ba37b3f37dfe77
jungsekwun <jungsekwun@192.168.0.20>
2021-09-23 17:49:51 +0900

    new Data

--- src/lessonOne/HelloWorld.java
+++ src/lessonOne/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World~!");
 		System.out.println("My name is tho" + " and you?");
+		System.out.println("Add new file");
 	}
 }
